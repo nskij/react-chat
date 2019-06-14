30ca2aef08c91b048125cb4912b643476c0e73f6
nskij <n.skij0123@gmail.com>
2019-06-15 03:15:47 +0900

    簡易実装deployまで

---
+++ build/precache-manifest.7ea246c948df349593cfc66ca236b131.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b29aaf71e72be8681ba7b87d2fda88de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9989a9fb825a015209ff",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "018a1630d07d62339c97",
+    "url": "/static/js/2.3baa1988.chunk.js"
+  },
+  {
+    "revision": "9989a9fb825a015209ff",
+    "url": "/static/js/main.e6ccb67e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAbgrKpystYAy6PZcJYBNkL-AuhWUQKrTg",
+  authDomain: "react-chat-37de6.firebaseapp.com",
+  databaseURL: "https://react-chat-37de6.firebaseio.com",
+  projectId: "react-chat-37de6",
+  storageBucket: "react-chat-37de6.appspot.com",
+  messagingSenderId: "846956337342"
+};
