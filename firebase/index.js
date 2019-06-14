9e31e51814379556fe477ab3efd6be7a1887b15b
nskij <n.skij0123@gmail.com>
2019-06-15 02:03:41 +0900

    init

--- firebase/index.js
+++
@@ -1,5 +0,0 @@
-import firebase  from 'firebase';
-import { firebaseConfig } from './firebase/config.js';
-
-export const firebaseApp = firebase.initializeApp(firebaseConfig);
-export const firebaseDb = firebaseApp.database();
