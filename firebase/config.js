2f65a98addd9d5bd17a020491dfc42a61caf0d09
nskij <nskij0123@gmail.com>
2019-06-14 21:57:16 +0900

    connect firebase

---
+++ firebase/config.js
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAbgrKpystYAy6PZcJYBNkL-AuhWUQKrTg",
+  authDomain: "react-chat-37de6.firebaseapp.com",
+  databaseURL: "https://react-chat-37de6.firebaseio.com",
+  storageBucket: "react-chat-37de6.appspot.com",
+  messagingSenderId: "846956337342"
+};
